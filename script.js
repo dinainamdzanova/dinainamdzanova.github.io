3ab7d3895779b836cbc5b0dcf1fe4eeb80f2f0ee
dinainamdzanova <83198883+dinainamdzanova@users.noreply.github.com>
2021-08-11 16:14:50 +0500

    Personal Portfolio

---
+++ script.js
@@ -0,0 +1,73 @@
+// Toggle Navbar 
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+    hideSection();
+    toggleNavbar();
+    document.body.classList.toggle("hide-scrolling");
+})
+function hideSection() {
+    document.querySelector("section.active").classList.toggle("fade-out");
+}
+function toggleNavbar() {
+    document.querySelector(".header").classList.toggle("active");
+}
+
+
+
+// Active Section 
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+        if (e.target.classList.contains("nav-item")) {
+            toggleNavbar();
+        }
+        else {
+            hideSection();
+            // document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(() => {
+            document.querySelector("section.active").classList.remove("active", "fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0,0);
+            document.body.classList.remove("hide-scrolling");
+        }, 500)
+    }
+})
+
+
+
+
+// Portfolio item detailes popup 
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("view-project-btn")) {
+        togglePortfolioPopup();
+        document.querySelector(".portfolio-popup").scrollTo(0,0);
+        portfolioItemDetailes(e.target.parentElement);
+    }
+})
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio-popup").classList.toggle("open");
+    document.body.classList.toggle("hide-scrolling");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
+
+document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup);
+
+
+
+// hide popup when clicking outside of it
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("pp-inner")) {
+        togglePortfolioPopup();
+    }
+}) 
+
+function portfolioItemDetailes(portfolioItem) {
+    document.querySelector(".pp-thumbnail img").src = 
+    portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+
+    document.querySelector(".pp-header h3").innerHTML = 
+    portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+
+    document.querySelector(".pp-body").innerHTML =
+    portfolioItem.querySelector(".portfolio-item-detailes").innerHTML;
+}
